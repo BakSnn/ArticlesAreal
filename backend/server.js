449afa81095116a4481f9a1572e152505a3a5e8c
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-11 16:31:21 +0300

    added nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 const sequelize = require("./db.js");
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: __dirname + "/.env.local" });
 
 const app = express();
 app.use(express.json());
+
 
 const startServer = async () => {
   try {
     await sequelize.authenticate();
     sequelize.sync();
-    app.listen(5000, () =>
+    app.listen(process.env.SERVER_PORT || 5000, () =>
       console.log("Сервер запущен на http://localhost:5000")
     );
     console.log("Подключение к БД установлено");
