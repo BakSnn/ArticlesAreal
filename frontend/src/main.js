63764573fb3d04fcbd30dc2878a794fa963543fb
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-13 10:42:17 +0300

    added ssr for vuetify and added comments in code

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,23 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-
-import router from "./router/routes";
-import store from "./store/store";
+// Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+// Components
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+// Store and router
+import router from "./router/routes";
+import store from "./store/store";
+
+// Added vuetify
 const vuetify = createVuetify({
   components,
   directives,
+  ssr: true,
 });
 
 createApp(App).use(vuetify).use(router).use(store).mount("#app");
