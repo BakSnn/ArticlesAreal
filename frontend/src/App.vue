1ecddedfb0f952dcb0019f808cb30f718de9b21b
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-14 00:14:12 +0300

    added navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,20 +3,19 @@
 <template>
   <v-app>
     <v-bottom-navigation :elevation="0" grow mode="shift">
-      <v-btn value="main" to="/">
+      <v-btn value="Главная" to="/">
         <v-icon>mdi-home</v-icon>
         <span>Главная</span>
       </v-btn>
-      <v-btn value="recent" to="/about">
+      <v-btn value="Статьи" to="/articles">
+        <v-icon>mdi-list-box-outline</v-icon>
+
+        <span>Статьи</span>
+      </v-btn>
+      <v-btn value="Добавление статьи" to="/addArticle">
         <v-icon>mdi-plus-box</v-icon>
 
-        <span>Добавить статью</span>
-      </v-btn>
-
-      <v-btn value="favorites">
-        <v-icon>mdi-map</v-icon>
-
-        <span>Favorites</span>
+        <span class="text-center">Добавление статьи</span>
       </v-btn>
 
       <v-btn value="nearby" to="">
