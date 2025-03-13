436014f865269db710767feb9d9db6c3b2fd19a8
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-13 17:39:48 +0300

    added navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-bottom-navigation :elevation="0" grow mode="shift">
+      <v-btn value="main" to="/">
+        <v-icon>mdi-home</v-icon>
+        <span>Главная</span>
+      </v-btn>
+      <v-btn value="recent" to="/about">
+        <v-icon>mdi-plus-box</v-icon>
+
+        <span>Добавить статью</span>
+      </v-btn>
+
+      <v-btn value="favorites">
+        <v-icon>mdi-map</v-icon>
+
+        <span>Favorites</span>
+      </v-btn>
+
+      <v-btn value="nearby" to="">
+        <v-icon>mdi-map-marker</v-icon>
+
+        <span>Nearby</span>
+      </v-btn>
+    </v-bottom-navigation>
+
+    <router-view />
+  </v-app>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
