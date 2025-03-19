ea88934e697e3c0b5540b56c8b99e9a8e3d4e964
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-19 19:09:48 +0300

    added a component for viewing, editing, deleting an article added a component for adding comments to an article

--- frontend/src/components/Articles.vue
+++ frontend/src/components/Articles.vue
@@ -1,7 +1,99 @@
-<script setup></script>
+<script setup>
+import { onMounted, computed, ref } from "vue";
+import { useStore } from "vuex";
+import Comments from "./Comments.vue";
+const store = useStore();
+
+const isEditing = ref(false);
+const currentArticle = ref(null);
+const updatedTitle = ref("");
+const updatedContent = ref("");
+
+onMounted(() => {
+  store.dispatch("articles/loadArticles");
+});
+
+const articles = computed(() => store.state.articles.articles);
+
+function removeArticle(id) {
+  store.dispatch("articles/removeArticle", id);
+}
+
+function editArticle(article) {
+  currentArticle.value = article;
+  updatedTitle.value = article.title;
+  updatedContent.value = article.content;
+  isEditing.value = true;
+}
+
+async function saveArticle() {
+  const articleData = {
+    title: updatedTitle.value,
+    content: updatedContent.value,
+  };
+
+  await store.dispatch("articles/updateExistingArticle", {
+    articleId: currentArticle.value.id,
+    articleData,
+  });
+  await store.dispatch("articles/loadArticles");
+  isEditing.value = false;
+  currentArticle.value = null;
+}
+
+function cancelEdit() {
+  isEditing.value = false;
+  currentArticle.value = null;
+}
+</script>
 
 <template>
-    
+  <v-container class="d-flex flex-column align-center justify-center">
+    <v-table class="text-center" max-width="800">
+      <thead>
+        <tr>
+          <th class="text-center">Заголовок</th>
+          <th class="text-center">Контент</th>
+          <th class="text-center">Рередактировать</th>
+          <th class="text-center">Удалить</th>
+          <th class="text-center">Добавить комментарий</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="article in articles" :key="article.id">
+          <td class="text-center">{{ article.title }}</td>
+          <td class="text-center">{{ article.content }}</td>
+          <td class="text-center">
+            <v-btn @click="editArticle(article)" color="green"
+              ><v-icon>mdi-note-edit-outline</v-icon></v-btn
+            >
+          </td>
+          <td class="text-center">
+            <v-btn @click="removeArticle(article.id)" color="red-lighten-1"
+              ><v-icon>mdi-delete-forever</v-icon></v-btn
+            >
+          </td>
+          <td>
+            <Comments :articleId="article.id" />
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+
+    <v-dialog v-model="isEditing" max-width="500px">
+      <v-card>
+        <v-card-title>Редактировать статью</v-card-title>
+        <v-card-text>
+          <v-text-field v-model="updatedTitle" label="Заголовок" />
+          <v-textarea v-model="updatedContent" label="Контент" rows="4" />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn @click="cancelEdit" color="red">Отмена</v-btn>
+          <v-btn @click="saveArticle" color="green">Сохранить</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/Comments.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const props = defineProps({
+  articleId: Number,
+});
+
+const isAddingAComment = ref(false);
+const isCommentAdded = ref(false);
+const newComment = ref("");
+
+function openDialogCommentAdded() {
+  isCommentAdded.value = true;
+}
+function closeDialogCommentAdded() {
+  isCommentAdded.value = false;
+}
+function openDialog() {
+  isAddingAComment.value = true;
+}
+
+function closeDialog() {
+  isAddingAComment.value = false;
+}
+
+async function addComment() {
+  await store.dispatch("comments/createNewComment", {
+    articleId: props.articleId,
+    commentData: { text: newComment.value, articleId: props.articleId },
+  });
+  closeDialog();
+  openDialogCommentAdded();
+}
+</script>
+
+<template>
+  <v-btn @click="openDialog">X+</v-btn>
+
+  <v-dialog v-model="isAddingAComment" width="500">
+    <v-card>
+      <v-card-title>Добавить комментарий</v-card-title>
+      <v-card-text>
+        <v-textarea v-model="newComment" label="Текст комментария"></v-textarea>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="closeDialog" color="red">Закрыть</v-btn>
+        <v-btn @click="addComment" color="green">Добавить</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog v-model="isCommentAdded" width="400">
+    <v-card>
+      <v-card-text>
+        <v-card-title class="d-flex justify-center"
+          >Комментарий добавлен</v-card-title
+        >
+      </v-card-text>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn @click="closeDialogCommentAdded" color="green">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
