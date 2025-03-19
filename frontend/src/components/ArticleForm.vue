1222cf37cfc7e2140f31c7f70fd83d44c5d84105
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-19 18:41:40 +0300

    added modal windows

--- frontend/src/components/ArticleForm.vue
+++ frontend/src/components/ArticleForm.vue
@@ -6,7 +6,26 @@ const title = ref("");
 const content = ref("");
 const store = useStore();
 
+const isAddingArticle = ref(false);
+const isEmpty = ref(false);
+
+function closeIsEmptyDialog() {
+  isEmpty.value = false;
+}
+
+function openDialog() {
+  isAddingArticle.value = true;
+}
+
+function closeDialog() {
+  isAddingArticle.value = false;
+}
+
 function submit() {
+  if (title.value === "" || content.value === "") {
+    isEmpty.value = true;
+    return;
+  } else openDialog();
   store.dispatch("articles/createNewArticle", {
     title: title.value,
     content: content.value,
@@ -16,7 +35,7 @@ function submit() {
 
 <template>
   <v-container>
-    <div style="width: 200vh; max-width: 100%">
+    <div style="width: 100vh; min-width: 100vh">
       <v-form>
         <v-text-field v-model="title" label="Заголовок" required></v-text-field>
         <v-textarea v-model="content" label="Содержание" required></v-textarea>
@@ -26,5 +45,25 @@ function submit() {
       </v-form>
     </div>
   </v-container>
+
+  <v-dialog v-model="isAddingArticle" width="200">
+    <v-card>
+      <v-card-title class="text-center">Добавлено</v-card-title>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn @click="closeDialog" color="success">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog v-model="isEmpty" width="400">
+    <v-card>
+      <v-card-title class="text-center"
+        >Статья не может быть пустой</v-card-title
+      >
+      <v-card-actions class="d-flex justify-center">
+        <v-btn color="success" @click="closeIsEmptyDialog">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 <style scoped></style>
