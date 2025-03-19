1b41dfdf4b326c40983ef2d55cc422771441d71f
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-19 19:10:25 +0300

    added label

--- frontend/src/views/ArticlesView.vue
+++ frontend/src/views/ArticlesView.vue
@@ -1,8 +1,12 @@
 <script setup>
 import Articles from "../components/Articles.vue";
 </script>
+
 <template>
-  <v-container>
-    <Articles />
+  <v-container fluid>
+    <v-row justify="center" align="center" class="fill-height">
+      <h1 class="text-center">Список статей</h1>
+      <Articles />
+    </v-row>
   </v-container>
 </template>
