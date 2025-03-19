5438c7b23514179689a6b901b969f81f26636fbd
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-19 19:10:40 +0300

    deleted last btn

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,27 @@
 <script setup></script>
 
-<template>
+<template v-slot:input="{ on }">
   <v-app>
-    <v-bottom-navigation :elevation="0" grow mode="shift">
-      <v-btn value="Главная" to="/">
-        <v-icon>mdi-home</v-icon>
-        <span>Главная</span>
-      </v-btn>
-      <v-btn value="Статьи" to="/articles">
-        <v-icon>mdi-list-box-outline</v-icon>
+    <v-container>
+      <v-bottom-navigation :elevation="0" grow mode="shift">
+        <v-btn value="Главная" to="/">
+          <v-icon>mdi-home</v-icon>
+          <span>Главная</span>
+        </v-btn>
+        <v-btn value="Статьи" to="/articles">
+          <v-icon>mdi-list-box-outline</v-icon>
 
-        <span>Статьи</span>
-      </v-btn>
-      <v-btn value="Добавление статьи" to="/addArticle">
-        <v-icon>mdi-plus-box</v-icon>
+          <span>Статьи</span>
+        </v-btn>
+        <v-btn value="Добавление статьи" to="/add-article">
+          <v-icon>mdi-plus-box</v-icon>
 
-        <span class="text-center">Добавление статьи</span>
-      </v-btn>
+          <span class="text-center">Добавление статьи</span>
+        </v-btn>
+      </v-bottom-navigation>
 
-      <v-btn value="nearby" to="">
-        <v-icon>mdi-map-marker</v-icon>
-
-        <span>Nearby</span>
-      </v-btn>
-    </v-bottom-navigation>
-
-    <router-view />
+      <router-view />
+    </v-container>
   </v-app>
 </template>
 
