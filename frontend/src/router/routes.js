3a87ae70e75972e55085d90c770081ee1ba61d82
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-21 23:54:29 +0300

    added new route

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import App from "../App.vue";
 import AddArticleViews from "../views/AddArticleView.vue";
 import ArticlesView from "../views/ArticlesView.vue";
+import ArticleDetailView from "../views/ArticleDetailView.vue";
 
 const routes = [
   { path: "/", component: App },
   { path: "/add-article", component: AddArticleViews },
   { path: "/articles", component: ArticlesView },
+  { path: "/article/:id", component: ArticleDetailView, name: "ArticleDetail" },
 ];
 
 const router = createRouter({
