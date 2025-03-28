c98f279cc23a14a245c1ab82256a081040971d74
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-28 22:08:03 +0300

    fixed height

--- frontend/src/views/AddArticleView.vue
+++ frontend/src/views/AddArticleView.vue
@@ -3,7 +3,7 @@ import ArticleForm from "../components/ArticleForm.vue";
 </script>
 
 <template>
-  <v-container class="d-flex justify-center align-center h-screen">
+  <v-container class="d-flex justify-center align-center full-height">
     <div>
       <h2 class="text-center mb-4">Добавление статьи</h2>
       <ArticleForm />
